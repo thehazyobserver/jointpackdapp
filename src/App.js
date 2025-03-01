47a0ac72e9adc715a2b4516a72e608ac10c3d36a
thehazyobserver <114879476+thehazyobserver@users.noreply.github.com>
2025-03-01 17:54:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,9 +90,9 @@ const MoreJointPacksButton = styled.button`
   padding: 10px;
   border-radius: 5px;
   border: none;
-  background-color: #121212; /* Change the button color here */
+  background-color: #ffffff; /* Change the button color here */
   font-weight: bold;
-  color: white;
+  color: #0059d7;
   cursor: pointer;
   transition: background-color 0.3s ease;
   margin-top: 10px; /* Added margin-top for spacing */
